ab929ce7f44910613e6c6ce2722d1018f3a11ead
AkashPulluru <akashpulluru@gmail.com>
2023-08-02 17:17:13 -0400

    almost done

--- practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/controllers/users_controller.rb
@@ -5,11 +5,32 @@ class UsersController < ApplicationController
         render json: User.all
     end
 
-    def create 
-        render json: params
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy 
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(params.require(:user).permit(:name, :email))
+            redirect_to user_url(@user.id)
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end 
     end 
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user 
+    end
+
+    def create 
+        @user = User.new(params.require(:user).permit(:name, :email))
+        # replace the `user_attributes_here` with the actual attribute keys
+        if @user.save
+            redirect_to user_url(@user.id)
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 end
