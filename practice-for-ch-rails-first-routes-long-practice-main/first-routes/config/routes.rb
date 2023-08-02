99512a311a089654167cdf35d0daa2383fba391d
AkashPulluru <akashpulluru@gmail.com>
2023-08-02 15:30:19 -0400

    onto postman

--- practice-for-ch-rails-first-routes-long-practice-main/first-routes/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first-routes/config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # resources :users
-  get 'users/:id', to 'users#show', as: 'user'
-  get 'users', to 'users#index', as: 'users'
-  post 'users', to 'users#create', as: 'users'
-  get 'users/new', to 'users#new', as: 'new_user'
-  get 'users/:id/edit', to 'users#edit', as: 'edit_user'
-  patch 'users/:id', to 'users#update', as: 'user'
-  put 'users/:id', to 'users#update', as: 'user'
-  delete 'users/:id', to 'users#destroy', as: 'user'
+  
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 end
