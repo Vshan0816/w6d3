e04070d5e69f9efeb7358836d6cff6faede2f903
Vincent <vshan0816@gmail.com>
2023-08-02 16:43:23 -0400

    Finish index with users

--- practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/controllers/users_controller.rb
@@ -2,10 +2,14 @@
 
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
     end
 
     def create 
         render json: params
     end 
+
+    def show
+        render json: params
+    end
 end
