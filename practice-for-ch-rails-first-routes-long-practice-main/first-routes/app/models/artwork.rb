ae0ea7507ef5da701226a16361835cf6cf7fab93
AkashPulluru <akashpulluru@gmail.com>
2023-08-03 11:02:45 -0400

    onto seeds

--- practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/models/artwork.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first-routes/app/models/artwork.rb
@@ -6,8 +6,17 @@ class Artwork < ApplicationRecord
     validates :artist_id, presence: true
     validates :title, uniqueness: {scope: :artist_id, message: "artist can not have 2 pieces of same title"}
 
-    belongs to: artist,
+    belongs_to :artist,
         primary_key: :id,
         foreign_key: :artist_id,
         class_name: :User
+    
+    has_many :artwork_shares,
+        primary_key: :id, 
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare 
+    
+    has_many :shared_viewers,
+        through: :artwork_shares, 
+        source: :viewer 
 end
--- practice-for-ch-rails-first-routes-long-practice-main/first-routes/db/migrate/20230802202255_create_user.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first-routes/db/migrate/20230802202255_create_user.rb
@@ -1,4 +1,4 @@
-class CreateUser < ActiveRecord::Migration[7.0]
+xclass CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, null: false
